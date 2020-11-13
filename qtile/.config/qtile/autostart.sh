ffbea34879785487154aab66715bea7301900525
Vidianos Giannitsis <vidianosgiannitsis@gmail.com>
2020-11-13 22:46:20 -0100

    Added brave Switched background for desktop and pdfs

--- qtile/.config/qtile/autostart.sh
+++ qtile/.config/qtile/autostart.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 setxkbmap -model pc104 -layout us,gr -variant ,, -option grp:alt_shift_toggle &
-nitrogen --set-scaled ~/Pictures/aurora.jpg &
+nitrogen --set-scaled ~/Pictures/Sol_manjaro.png &
 nm-applet &
 picom --config ~/.config/picom.conf &
