885eb35156a33772c22741088687cd341301d460
Vidianos-Giannitsis <vidianosgiannitsis@gmail.com>
2022-01-12 21:08:56 +0200

    Reorganised my emacs config so its more neat.
    Created a new file Zettelkasten.org which stores all my org roam and
    references etc. code snippets. That section was getting really large
    so I wanted to split it.

---
+++ emacs/.emacs.d/elpa/devdocs-0.2/devdocs-pkg.el
@@ -0,0 +1,2 @@
+;; Generated package description from devdocs.el  -*- no-byte-compile: t -*-
+(define-package "devdocs" "0.2" "Emacs viewer for DevDocs" '((emacs "27.1")) :keywords '("help") :authors '(("Augusto Stoffel" . "arstoffel@gmail.com")) :maintainer '("Augusto Stoffel" . "arstoffel@gmail.com") :url "https://github.com/astoff/devdocs.el")
